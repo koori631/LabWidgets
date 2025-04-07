8416b51ce241f89423aebb4b9c9997c9f1915711
gulevich danil <danilgulevi4@gmail.com>
2025-04-07 16:34:51 +0700

    Code Implementation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
-#include "mainwindow.h"
 
 #include <QApplication>
 
+#include "win.h"
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    //some comments
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    Win win(0);
+    win.show();
+    return app.exec();
 }
